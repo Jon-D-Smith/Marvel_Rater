f1ed5fa7fb88c0bb1054571ecf6e739df4ac3f23
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-13 23:38:14 -0600

    Updated Schema, cleaned up css, sortby cronological order

--- index.js
+++ index.js
@@ -1,62 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const methodOverride = require('method-override');
-const path = require('path');
-const Movie = require('./models/movie')
+const methodOverride = require("method-override");
+const path = require("path");
+const Movie = require("./models/movie");
 
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 //App use
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(__dirname + '/public'));
-
+app.use(methodOverride("_method"));
+app.use(express.static(__dirname + "/public"));
 
 //Redirect to main movies page (Just in case I expand the app)
-app.get('/', (req, res) => {
-    res.redirect('/movies')
-})
+app.get("/", (req, res) => {
+  res.redirect("/movies");
+});
 
 //Getting Home Page
-app.get('/movies', async (req, res) => {
-    const movies = await Movie.find({})
-    res.render('home', { movies })
-})
+app.get("/movies", async (req, res) => {
+  const movies = await Movie.find({}).sort("order");
+  res.render("home", { movies });
+});
 
 //New Movies
-app.get('/movies/new', (req, res) => {
-    res.render('new')
-})
+app.get("/movies/new", (req, res) => {
+  res.render("new");
+});
 
-app.post('/movies', async (req, res) => {
-    const movie = await new Movie(req.body);
-    movie.save()
-    res.redirect('/movies')
-})
+app.post("/movies", async (req, res) => {
+  const movie = await new Movie(req.body);
+  movie.save();
+  res.redirect("/movies");
+});
 
 //Edit Movie Rating
-app.get('/movies/:id/edit', async (req, res) => {
-    const { id } = req.params;
-    const movie = await Movie.findById(id);
-    res.render('edit', { movie })
-})
+app.get("/movies/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const movie = await Movie.findById(id);
+  res.render("edit", { movie });
+});
 
-app.put('/movies/:id', async (req, res) => {
-    const { id } = req.params;
-    const movie = await Movie.findByIdAndUpdate(id, req.body)
-    res.redirect('/movies')
-})
+app.put("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+  const movie = await Movie.findByIdAndUpdate(id, req.body);
+  res.redirect("/movies");
+});
 
 //Delete Route
-app.delete('/movies/:id', async (req, res) => {
-    const { id } = req.params;
-    const movie = await Movie.findByIdAndDelete(id)
-    res.redirect('/movies')
-})
-
+app.delete("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+  const movie = await Movie.findByIdAndDelete(id);
+  res.redirect("/movies");
+});
 
 app.listen(PORT, () => {
-    console.log(`Connected on port ${PORT}`)
-})
+  console.log(`Connected on port ${PORT}`);
+});
--- models/movie.js
+++ models/movie.js
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/MarvelRater',
-    { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("Connected to mongoose"));
+mongoose
+  .connect("mongodb://localhost:27017/MarvelRater", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to mongoose"));
 
 const movieSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    comedy: Number,
-    immersion: Number,
-    eggs: Number,
-    emotion: Number,
-    development: Number
-})
+  title: String,
+  image: String,
+  order: Number,
+  comedy: Number,
+  immersion: Number,
+  eggs: Number,
+  emotion: Number,
+  development: Number,
+});
 
-const Movie = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model("Movie", movieSchema);
 
 module.exports = Movie;
